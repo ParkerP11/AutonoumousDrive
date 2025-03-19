a92ae2bd62e54e68ba6f18e8dd9a026dd2e658b5
PP <mathewpruitt03793@gmail.com>
2025-03-19 15:23:02 -0500

    Added the foundation for the Autonomous Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDrive.java
@@ -1,17 +1,251 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorControllerEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+import java.util.ArrayList;
+
+
+/*
+
+Parker Pruitt, 2025
+FTC team Ubett 8672
+
+A autonomous library for movement based on position
+using the Goblida Pinpoint Co-Processor
+
+The coordinate System is (0,0) is the left corner of your starting side
+Each tile is 24 inches
+X is vertical forward backward axis
+Y is the horizontal side to side axis
+Standard (x,y) -> Our coordinates are (y,x)
+
+ */
 public class AutonomousDrive {
+
+    //Error Tolerances
+    private final double POSE_ERROR_TOLERANCE = 0.01;
+    private final double HEADING_ERROR_TOLERANCE = 0.01;
+
+    private final double MAX_MOTOR_CURRENT = 9.5;
     //PID controls
+
+    //For Drive Movement
+    private static double kDP = 1;
+    private static double kDI = 0;
+    private static double kDD = 0;
+    private static double errorSumD = 0;
+    private static double errorSumRangeD = 5;
+
+    //For Turn Movement
+    private static double kTP = 1;
+    private static double kTI = 0;
+    private static double kTD = 0;
+    private static double errorSumT = 0;
+    private static double errorSumRangeT = 5;
 
 
 
     //Drive motor names
+    private String leftFrontName = "lf";
+    private String leftBackName = "lb";
+    private String rightFrontName = "rf";
+    private String rightBackName = "rb";
 
+    //Drive motor vars
+    private static DcMotorEx lf;
+    private static DcMotorEx lb;
+    private static DcMotorEx rf;
+    private static DcMotorEx rb;
 
+    private static int leftFrontNum;
+    private static int leftBackNum;
+    private static int rightFrontNum;
+    private static int rightBackNum;
 
+    private static DcMotorControllerEx motorControllerEx;
+
+    private static double[] motorCurrents = new double[4];
 
     //Gobilda Pinpoint
+    private String odoName = "odo";
+    private GoBildaPinpointDriver odo;
+
+    //Pinpoint offsets from Center in mm and encoder Direction
+    //Left is +X, Front is +Y
+    private double xoffset = 100;
+    private double yoffset = 100;
+
+    private GoBildaPinpointDriver.EncoderDirection xDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+    private GoBildaPinpointDriver.EncoderDirection yDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+
+    //Dead Wheel Type
+
+    private GoBildaPinpointDriver.GoBildaOdometryPods encoderPod = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+
+    //LinearOpmode
+
+    private LinearOpMode opMode;
 
 
-    //The
+    //Start Positions as {y,x} format and add to the master list for poses
+
+    private double[] pos1 = new double[] {0,0, 0};
+    private double[] pos2 = new double[] {8,36, 0 };
+
+
+    private double[][] masterStartPoses = new double[][] { pos1, pos2};
+
+
+
+
+
+    //Default Constructor
+    public AutonomousDrive(LinearOpMode opMode, int startPos){
+        this.opMode = opMode;
+
+        lf = opMode.hardwareMap.get(DcMotorEx.class, leftFrontName);
+        lb = opMode.hardwareMap.get(DcMotorEx.class, leftBackName);
+        rf = opMode.hardwareMap.get(DcMotorEx.class, rightFrontName);
+        rb = opMode.hardwareMap.get(DcMotorEx.class, rightBackName);
+
+        leftFrontNum = lf.getPortNumber();
+        leftBackNum = lb.getPortNumber();
+        rightFrontNum = rf.getPortNumber();
+        rightBackNum = rb.getPortNumber();
+
+        motorControllerEx = (DcMotorControllerEx)(lf.getController());
+
+        motorCurrents = getMotorCurrents();
+
+        motorControllerEx.setMotorCurrentAlert(leftFrontNum, MAX_MOTOR_CURRENT, CurrentUnit.AMPS);
+        motorControllerEx.setMotorCurrentAlert(leftBackNum, MAX_MOTOR_CURRENT, CurrentUnit.AMPS);
+        motorControllerEx.setMotorCurrentAlert(rightFrontNum, MAX_MOTOR_CURRENT, CurrentUnit.AMPS);
+        motorControllerEx.setMotorCurrentAlert(rightBackNum, MAX_MOTOR_CURRENT, CurrentUnit.AMPS);
+
+        odo = opMode.hardwareMap.get(GoBildaPinpointDriver.class, odoName);
+
+        //Pinpoint offsets from Center in mm
+        //Left is +X, Front is +Y
+        odo.setOffsets(xoffset, yoffset);
+        odo.setEncoderDirections(xDirection, yDirection);
+        odo.setEncoderResolution(encoderPod);
+
+        odo.recalibrateIMU();
+        opMode.sleep(50);
+        odo.resetPosAndIMU();
+        opMode.sleep(50);
+
+        odo.setPosition(new Pose2D(DistanceUnit.INCH, masterStartPoses[startPos][1],masterStartPoses[startPos][0], AngleUnit.DEGREES, masterStartPoses[startPos][0]));
+        opMode.sleep(10);
+    }
+
+
+    //Getters and Setters and basic functions
+    public static void drive(double rfPower, double rbPower, double lbPower, double lfPower) {
+
+        rf.setPower(rfPower);
+        rb.setPower(rbPower);
+        lb.setPower(lbPower);
+        lf.setPower(lfPower);
+    }
+    public GoBildaPinpointDriver getPinPoint(){return odo; }
+
+    public Pose2D getPos(){return  odo.getPosition(); }
+    public double getX(){return  odo.getPosition().getX(DistanceUnit.INCH); }
+    public double getY(){return  odo.getPosition().getY(DistanceUnit.INCH); }
+
+    public double getHeading(){
+        double rawHeading = odo.getPosition().getHeading(AngleUnit.DEGREES);
+        return  rawHeading + 180;
+    }
+
+    public double getTargetHeadDist(double targetHeading){
+        targetHeading = Math.abs(targetHeading) % 360;
+
+        double currentHeading = getHeading();
+        double angleTogo = targetHeading - currentHeading;
+        currentHeading =  getHeading();
+
+        angleTogo = targetHeading - currentHeading;
+
+        if(Math.abs(angleTogo) > 180) {
+            if (currentHeading < 180) {
+                angleTogo = -((currentHeading) + (360 - targetHeading));
+            } else {
+                angleTogo = (targetHeading + (360 - currentHeading));
+            }
+        }
+        return angleTogo;
+    }
+
+    public static double[] getMotorCurrents(){
+        double currentLF = motorControllerEx.getMotorCurrent(leftFrontNum, CurrentUnit.AMPS);
+        double currentLB = motorControllerEx.getMotorCurrent(leftBackNum, CurrentUnit.AMPS);
+        double currentRF = motorControllerEx.getMotorCurrent(rightFrontNum, CurrentUnit.AMPS);
+        double currentRB = motorControllerEx.getMotorCurrent(rightBackNum, CurrentUnit.AMPS);
+        return new double[] {currentRF, currentRB, currentLB, currentLF};
+    }
+
+    public static ArrayList<Boolean> isMotorsOver(){
+        boolean overCurrentLF = motorControllerEx.isMotorOverCurrent(leftFrontNum);
+        boolean overCurrentLB = motorControllerEx.isMotorOverCurrent(leftBackNum);
+        boolean overCurrentRF = motorControllerEx.isMotorOverCurrent(rightFrontNum);
+        boolean overCurrentRB = motorControllerEx.isMotorOverCurrent(rightBackNum);
+
+        ArrayList<Boolean> list = new ArrayList<>();
+
+        list.add(overCurrentLF);
+        list.add(overCurrentLB);
+        list.add(overCurrentRF);
+        list.add(overCurrentRB);
+
+        return list;
+    }
+
+    //PIDs
+
+    private static double movePID(double error){
+        double output = error * kDP - error * kDD + errorSumD*kDI;
+        if(Math.abs(error) <= errorSumRangeD){
+            errorSumD += error;
+        }
+        return output;
+    }
+
+    private static double turnPID(double error){
+        double output = error * kTP - error * kTD + errorSumT*kTI;
+        if(Math.abs(error) <= errorSumRangeT){
+            errorSumT += error;
+        }
+        return output;
+    }
+
+    //Movement
+    public void goToPointConstantHeading(double targetX, double targetY){
+        odo.update();
+
+        double targetXDist = targetX - getX();
+        double targetYDist = targetY - getY();
+        double angle = Math.atan2(targetXDist, targetYDist);
+
+
+        double startHeading = getHeading();
+        double turn = turnPID(startHeading);
+
+        double v1;
+        double v2;
+        double v3;
+        double v4;
+
+
+
+
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpModes/TestMotors.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.testOpModes;
+
+public class TestMotors {
+}
